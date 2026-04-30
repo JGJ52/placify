fb1c97c7c9e9a3926270b0c5d1f5a81afdb3450b
jgj52 <email@jgj52.hu>
2026-05-01 00:44:01 +0200

    very small changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,16 +2,22 @@
 import "./navbar.css";
 import Button from "./button/button";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPeopleGroup} from "@fortawesome/free-solid-svg-icons";
+import {faList, faPeopleGroup} from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
     return (
         <>
             <nav>
-                <Button>
-                    <FontAwesomeIcon icon={faPeopleGroup} />
-                    Artists
-                </Button>
+                <div className="flex flex-row">
+                    <Button>
+                        <FontAwesomeIcon icon={faPeopleGroup} />
+                        Artists
+                    </Button>
+                    <Button>
+                        <FontAwesomeIcon icon={faList} />
+                        Playlists
+                    </Button>
+                </div>
             </nav>
         </>
     )
